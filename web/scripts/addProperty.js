5a05149b681b19f4bbf27c6bf35f175232e7d481
bedroo <berdien.deroo@ugent.be>
2015-07-28 11:01:12 +0200

    Update insert form to allow multiproperties
    Updated the forms (nodeType and relationType) to allow adding multiple
    properties

--- web/scripts/addProperty.js
+++ web/scripts/addProperty.js
@@ -1,29 +1,38 @@
-//$(function() {
-	var collectionHolder;
+var collectionHolder;
+
+// setup an "add a tag" link
+var $addRelationLink = $('<a href="#" class="add_relation_link">Add another relation</a>');
+var $newLinkLi = $('<li></li>').append($addRelationLink);
+
+jQuery(document).ready(function() {
+	//get the ul that holds the collection of relations
+	$collectionHolder = $('ul.relations');
+	//add the 'add relation link' to the relations ul
+	$collectionHolder.append($newLinkLi);
 	
-	// setup an "add a tag" link
-	var $addRelationLink = $('<a href="#" class="add_relation_link">Add another relation</a>');
-	var $newLinkLi = $('<li></li>').append($addRelationLink);
+	//count the current list items, but decrease one as link is also list item, use that as the new inserting a new item
+	$collectionHolder.data('index', $collectionHolder.find($('li')).length - 1);
 	
-	jQuery(document).ready(function() {
-		$collectionHolder = $('ul.relations');
-		$collectionHolder.append($newLinkLi);
-		
-		$collectionHolder.data('index', $collectionHolder.find('inout').length);
-		
-		$addRelationLink.on('click', function(e){
-			e.preventDefault();
-			addRelationForm($collectionHolder, $newLinkLi);
-		});
+	$addRelationLink.on('click', function(e){
+		//prevent the link from creating a # on the url
+		e.preventDefault();
+		//add a new relations form
+		addRelationForm($collectionHolder, $newLinkLi);
 	});
+});
+
+function addRelationForm($collectionHolder, $newLinkLi) {
+	//get the data-prototype
+	var prototype = $collectionHolder.data('prototype');
+	//get the new index
+	var index = $collectionHolder.data('index');
 	
-	function addRelationForm($collectionHolder, $newLinkLi) {
-		var prototype = $collectionHolder.data('prototype');
-		var index = $collectionHolder.data('index');
-		//var newForm = prototype.replace(/___name___/g, index);
-		var newForm = prototype.replace(/__name__/g, index);
-		$collectionHolder.data('index', index + 1);
-		var $newFormLi = $('<li></li>').append(newForm);
-		$newLinkLi.before($newFormLi);
-	}
-//});
+	//replace als the '__name__' in the prototype's html to the index number
+	var newForm = prototype.replace(/__name__/g, index);
+	
+	//increase the index with one
+	$collectionHolder.data('index', index + 1);
+	//display the form in the page in a li, before the link
+	var $newFormLi = $('<li></li>').append(newForm);
+	$newLinkLi.before($newFormLi);
+}
