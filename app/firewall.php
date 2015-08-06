d5dce27bf316409f416c2df6f737238363b13f92
David Vandorpe <David.Vandorpe@UGent.be>
2015-08-06 16:14:27 +0200

    ADMIN, EDITOR and USER role

--- app/firewall.php
+++ app/firewall.php
@@ -13,7 +13,19 @@ $app['security.firewalls'] = array(
 		),
 		'logout' => array(
 			'logout_path' => '/user/logout',
+			'invalidate_session' => true
 		),
 		'users' => $app->share(function($app) { return $app['user.manager']; }),
 	),
+);
+
+$app['security.role_hierarchy'] = array(
+	'ROLE_ADMIN' => array('ROLE_EDITOR'),
+	'ROLE_EDITOR' => array('ROLE_USER')
+);
+
+$app['security.access_rules'] = array(
+	array('^/insert', 'ROLE_EDITOR'),
+	array('^/import', 'ROLE_EDITOR'),
+	array('^/update', 'ROLE_EDITOR'),
 );
