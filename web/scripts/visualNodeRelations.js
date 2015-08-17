5bb49e67d8f5bb7aca2460b667bcc2575f271548
David Vandorpe <David.Vandorpe@UGent.be>
2015-08-18 00:49:38 +0200

    Moved graph js to script file

--- web/scripts/visualNodeRelations.js
+++ web/scripts/visualNodeRelations.js
@@ -1,8 +1,90 @@
 /**
  * Created by Berdien De Roo on 17/08/2015.
  */
+/*
+ function createGraph(nodesJSON, linksJSON) {
+ //append an svg canvas to a DOM element
+ var vis = d3.select('#graph')
+ .append("svg");
 
-function createGraph(nodesJSON, linksJSON) {
+ //change width and height
+ var w = 900;
+ var h = 400;
+ vis.attr("width", w)
+ .attr("height", h);
+
+ //define nodes
+ var nodeMap = {};
+ var nodes = nodesJSON;
+ nodes.forEach(function (d) {
+ d.id = +d.id;
+ nodeMap[d.id] = d;
+ });
+
+ //add links
+ var links = linksJSON;
+ links.forEach(function (d) {
+ d.source = nodeMap[d.source],
+ d.target = nodeMap[d.target]
+ });
+
+ //use force directed layout algorithm
+ var force = d3.layout.force()
+ .size([w - 10, h - 10])
+ .charge(-100)
+ .linkDistance(20)
+ .nodes(nodes)
+ .links(links)
+ .start();
+
+ //visualize links as line
+ var link = vis.selectAll(".link")
+ .data(links)
+ .enter().append("line")
+ .attr("class", "link")
+ .style("stroke", "green")
+ .style("stroke-width", "5px");
+
+ //visualize nodes as circle with label
+ var node = vis.selectAll(".node")
+ .data(nodes, function (d) {
+ return d.id
+ })
+ .enter().append("g")
+ .attr("class", "node");
+
+ node.append("circle")
+ .attr("r", "5px")
+ .attr("fill", "black");
+
+ node.append("text")
+ .attr("dx", 12)
+ .attr("dy", ".35em")
+ .text(function (d) {
+ return d.name
+ });
+
+ //update the displayed positions of nodes and links on every tick of the simulation
+ force.on("tick", function () {
+ link.attr("x1", function (d) {
+ return d.source.x;
+ })
+ .attr("y1", function (d) {
+ return d.source.y;
+ })
+ .attr("x2", function (d) {
+ return d.target.x;
+ })
+ .attr("y2", function (d) {
+ return d.target.y;
+ });
+
+ node.attr("transform", function (d) {
+ return "translate(" + d.x + "," + d.y + ")";
+ })
+ })
+ }*/
+function createGraph(nodes, links) {
     //append an svg canvas to a DOM element
     var vis = d3.select('#graph')
         .append("svg");
@@ -15,37 +97,34 @@ function createGraph(nodesJSON, linksJSON) {
 
     //define nodes
     var nodeMap = {};
-    var nodes = nodesJSON;
     nodes.forEach(function (d) {
         d.id = +d.id;
         nodeMap[d.id] = d;
     });
 
-    //add links
-    var links = linksJSON;
     links.forEach(function (d) {
         d.source = nodeMap[d.source],
             d.target = nodeMap[d.target]
     });
 
-    //use force directed layout algorithm
+//use force directed layout algorithm
     var force = d3.layout.force()
-        .size([w - 10, h - 10])
+        .size([w - 50, h - 50])
         .charge(-100)
         .linkDistance(20)
         .nodes(nodes)
         .links(links)
         .start();
 
-    //visualize links as line
+//visualize links as line
     var link = vis.selectAll(".link")
         .data(links)
         .enter().append("line")
         .attr("class", "link")
-        .style("stroke", "green")
+        .style("stroke", "#00bc8c")
         .style("stroke-width", "5px");
 
-    //visualize nodes as circle with label
+//visualize nodes as circle with label
     var node = vis.selectAll(".node")
         .data(nodes, function (d) {
             return d.id
@@ -55,16 +134,18 @@ function createGraph(nodesJSON, linksJSON) {
 
     node.append("circle")
         .attr("r", "5px")
-        .attr("fill", "black");
+        .attr("fill", "white");
 
     node.append("text")
         .attr("dx", 12)
         .attr("dy", ".35em")
         .text(function (d) {
             return d.name
-        });
+        })
+        .style("font-size", "12px")
+        .style("fill", "white");
 
-    //update the displayed positions of nodes and links on every tick of the simulation
+//update the displayed positions of nodes and links on every tick of the simulation
     force.on("tick", function () {
         link.attr("x1", function (d) {
             return d.source.x;
@@ -82,5 +163,5 @@ function createGraph(nodesJSON, linksJSON) {
         node.attr("transform", function (d) {
             return "translate(" + d.x + "," + d.y + ")";
         })
-    })
+    });
 }
