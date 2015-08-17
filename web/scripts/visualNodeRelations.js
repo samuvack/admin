3a07c4fad58c4e0d43236ddc999c16411f08992d
David Vandorpe <David.Vandorpe@UGent.be>
2015-08-18 01:25:32 +0200

    Moved scripts to seperate file No clue where this diff is coming from, I thought I had this commited

--- web/scripts/visualNodeRelations.js
+++ web/scripts/visualNodeRelations.js
@@ -1,94 +1,16 @@
 /**
  * Created by Berdien De Roo on 17/08/2015.
+ * Improved by David Vandorpe on 18/08/2015
+ *      - Based on WiNA's notorious muilgraaf, written by Lorin Werthen and David Vandorpe (ZeusWPI pride)
+ *          - Yes, this is a thing.
  */
-/*
- function createGraph(nodesJSON, linksJSON) {
- //append an svg canvas to a DOM element
- var vis = d3.select('#graph')
- .append("svg");
 
- //change width and height
- var w = 900;
- var h = 400;
- vis.attr("width", w)
- .attr("height", h);
-
- //define nodes
- var nodeMap = {};
- var nodes = nodesJSON;
- nodes.forEach(function (d) {
- d.id = +d.id;
- nodeMap[d.id] = d;
- });
-
- //add links
- var links = linksJSON;
- links.forEach(function (d) {
- d.source = nodeMap[d.source],
- d.target = nodeMap[d.target]
- });
-
- //use force directed layout algorithm
- var force = d3.layout.force()
- .size([w - 10, h - 10])
- .charge(-100)
- .linkDistance(20)
- .nodes(nodes)
- .links(links)
- .start();
-
- //visualize links as line
- var link = vis.selectAll(".link")
- .data(links)
- .enter().append("line")
- .attr("class", "link")
- .style("stroke", "green")
- .style("stroke-width", "5px");
-
- //visualize nodes as circle with label
- var node = vis.selectAll(".node")
- .data(nodes, function (d) {
- return d.id
- })
- .enter().append("g")
- .attr("class", "node");
-
- node.append("circle")
- .attr("r", "5px")
- .attr("fill", "black");
-
- node.append("text")
- .attr("dx", 12)
- .attr("dy", ".35em")
- .text(function (d) {
- return d.name
- });
-
- //update the displayed positions of nodes and links on every tick of the simulation
- force.on("tick", function () {
- link.attr("x1", function (d) {
- return d.source.x;
- })
- .attr("y1", function (d) {
- return d.source.y;
- })
- .attr("x2", function (d) {
- return d.target.x;
- })
- .attr("y2", function (d) {
- return d.target.y;
- });
-
- node.attr("transform", function (d) {
- return "translate(" + d.x + "," + d.y + ")";
- })
- })
- }*/
 function createGraph(nodes, links) {
+
+
     //append an svg canvas to a DOM element
     var vis = d3.select('#graph')
         .append("svg");
-
     //change width and height
     var w = 900;
     var h = 400;
@@ -103,8 +25,8 @@ function createGraph(nodes, links) {
     });
 
     links.forEach(function (d) {
-        d.source = nodeMap[d.source],
-            d.target = nodeMap[d.target]
+        d.source = nodeMap[d.source];
+        d.target = nodeMap[d.target]
     });
 
 //use force directed layout algorithm
